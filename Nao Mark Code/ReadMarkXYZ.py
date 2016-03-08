1ce891e91d1c088b9114cc4bb84ec5f840c99cd2
MitchellDeplazes <mitchelldeplazes@gmail.com>
2016-03-08 14:56:53 -0700

    Update to work with new module

--- Nao Mark Code/ReadMarkXYZ.py
+++ Nao Mark Code/ReadMarkXYZ.py
@@ -3,12 +3,18 @@ from naoqi import ALProxy
 import NaoMarkModule
 
 #IP of the Robot
-IP = "10.0.0.6"
+IP = "10.0.0.7"
 #Port Number of the Robot
 PORT = 9559
 
 #Get the x,y,z in meters
-x, y, z = NaoMarkModule.getMarkXYZ(IP, PORT, 0.08)
+markData = NaoMarkModule.getMarkData(IP, PORT)
+
+#Get data until face is found
+while (markData is None or len(markData) == 0):
+    markData = NaoMarkModule.getMarkData(IP, PORT)
+
+x, y, z = NaoMarkModule.getMarkXYZ(IP, PORT, markData, 0.04)
 
 #Print information
 print "x " + str(x) + " (in meters)"
