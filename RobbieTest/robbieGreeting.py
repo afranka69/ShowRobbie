6f032d8a649d5acfb60d39063cd049c84b55842c
mdsuth <mdsutherland@mtech.edu>
2016-05-26 10:25:51 -0600

    xb8

--- RobbieTest/robbieGreeting.py
+++ RobbieTest/robbieGreeting.py
@@ -69,7 +69,7 @@ def main():
 
     id = cm.post.wave()
     cm.wait(id,0)
-    tts.say("Hello, I am Robbie. I'm a NAO robot. I'm designed and manufactured by the Aldebaan company in France, but all of my present behaviors have been programmed as part of a Senior Software Engineering project")
+    tts.say("Hello, I am Robbie. I'm a NAO robot. I'm designed and manufactured by the Aldebaan company in France, but all of my present behaviors have been programmed as part of a Senior Software Engineering project. Except for Josh Lee. He didn't do shit")
     tts.say("There's very little I can do without the programs that have designed and constructed by the Senior Software Engineering students here at Montana Tech. I'm afraid that programming me is not easy, but these students have been well equiped by their education here at Tech to deal with complex problems.")
 
 
